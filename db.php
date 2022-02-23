080271254219c7dc4ea5ed795db1eca68a94f37e
Dadezana <zatidavide@gmail.com>
2022-02-23 20:21:35 +0100

    Added db.php with connect() query() disconnect()

---
+++ db.php
@@ -0,0 +1,33 @@
+<?php
+    class DB{
+        protected $server, $db, $table;
+        private $user, $psw;
+        private $conn = NULL;
+        private $isConnected = false;
+
+        function DB($server, $db, $user="root", $psw=""){
+            $this->server = $server;
+            $this->db = $db;
+            $this->user = $user;
+            $this->psw = $psw;
+        }
+
+        function connect(){
+            $conn = mysqli_connect($this->server, $this->user, $this->psw, $this->db);
+            $isConnected = false;
+            if(!$conn) return false;
+
+            $isConnected = true;
+            return true;
+        }
+
+        function query($query){
+            return mysqli_query($this->conn, $query);
+        }
+
+        function disconnect(){
+            if(!$this->isConnected) return true;
+            return mysqli_close($this->conn);
+        }
+    }
+?>
